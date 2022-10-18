e347ab9ad3aa3bdf6dfaa0e025b33ac9331ed682
leyonel <leonellayonel@gmail.com>
2022-10-18 08:39:04 -0600

    clase 18-10-22

--- test/caracteristicas/juegos_jugados/repositorio_juagadas_test.dart
+++ test/caracteristicas/juegos_jugados/repositorio_juagadas_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_application_1/caracteristicas/juegos_jugados/repositorio_juagadas.dart';
+import 'package:flutter_application_1/dominio/juego_jugado.dart';
 import 'package:flutter_application_1/dominio/variable_dominio.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -11,7 +12,36 @@ void main() {
     resultado.match((l) {
       expect(true, equals(false));
     }, (r) {
-      expect(resultado.length(), equals(5));
+      expect(r.length, equals(5));
+    });
+  });
+
+  test('Takenoko es de los jugados por Benthor', () async {
+    RepositorioJuegosJugadosPruebas repositorio =
+        RepositorioJuegosJugadosPruebas();
+    final resultado = await repositorio
+        .obtenerJugadasRegistradas(NickFormado.constructor('Benthor'));
+    final takenoko = JuegoJugado.constructor(
+        nombrePropuesto: 'Takenoko', idPropuesto: '70919');
+    resultado.match((l) {
+      expect(true, equals(false));
+    }, (r) {
+      expect(r.contains(takenoko), equals(true));
+    });
+  });
+
+  test('Monopoly NO es de los jugados por Benthor', () async {
+    RepositorioJuegosJugadosPruebas repositorio =
+        RepositorioJuegosJugadosPruebas();
+    final resultado = await repositorio
+        .obtenerJugadasRegistradas(NickFormado.constructor('Benthor'));
+    final monopoly = JuegoJugado.constructor(
+        nombrePropuesto: 'Monopoly', idPropuesto: '918');
+    resultado.match((l) {
+      // expect(true, equals(false));
+      assert(false);
+    }, (r) {
+      expect(!r.contains(monopoly), equals(true));
     });
   });
 }
